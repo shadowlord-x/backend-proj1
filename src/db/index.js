3049387e7608cb52476bf3230d039fc73e6abfb0
shadowlord-x <mishravinayak047@gmail.com>
2026-04-09 13:34:04 +0530

    syntax cahnges

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import {DB_name} from '../constants.js';
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_name}`)
-    console.log(`\n MONGODB connected !! DB HOST: ${connectionInstance,connection.host}`); // This is done if we connect on some other server instead of production
+    console.log(`\n MONGODB connected !! DB HOST: ${connectionInstance.connection.host}`); // This is done if we connect on some other server instead of production
   } catch (error) {
     console.error("MongoDB connection error", error);
     process.exit(1)
